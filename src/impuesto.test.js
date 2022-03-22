b9cdb7284e226c4039b0f435b66440394ca11b80
karen <karen.aguayo@ucb.edu.bo>
2022-03-21 20:18:52 -0400

    segundo  test

--- src/impuesto.test.js
+++ src/impuesto.test.js
@@ -5,4 +5,7 @@ describe("TOTALIZADOR",()=>{
     it("devuelve el impuesto",()=>{
         expect(impuesto_total_ca(10000,6.65)).toEqual(665);
     });
+    it("devuelve el descuento",()=>{
+        expect(porcentaje_descuento(7000,5)).toEqual(0.35);
+    });
 });
